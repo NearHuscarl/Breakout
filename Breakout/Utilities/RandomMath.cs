696630b03a0b40c6f59b46987a01341381141b8b
NearHuscarl <16520846@gm.uit.edu.vn>
2018-04-28 18:49:19 +0700

    add random math helper

---
+++ Breakout/Utilities/RandomMath.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakout.Utilities
+{
+	public static class RandomMath
+	{
+		private static Random random;
+		public static Random Random
+		{
+			get
+			{
+				return random;
+			}
+		}
+
+		public static float RandomBetween(float minValue, float maxValue)
+		{
+			return minValue + (float)random.NextDouble() * (maxValue - minValue);
+		}
+
+		public static Vector2 RandomDirection()
+		{
+			float angle = RandomBetween(0, MathHelper.TwoPi);
+			Vector2 direction = new Vector2
+			{
+				X = (float)Math.Cos(angle),
+				Y = (float)Math.Sin(angle),
+			};
+
+			return direction;
+		}
+		
+		public static Vector2 RandomDirection(float minAngle, float maxAngle)
+		{
+			float angle = RandomBetween(MathHelper.ToRadians(minAngle), MathHelper.ToRadians(maxAngle)) - MathHelper.PiOver2;
+			Vector2 direction = new Vector2
+			{
+				X = (float)Math.Cos(angle),
+				Y = (float)Math.Sin(angle),
+			};
+
+			return direction;
+		}
+
+
+		public static bool RandomPercent(float percentage)
+		{
+			return random.NextDouble() * 100 < percentage;
+		}
+	}
+}
